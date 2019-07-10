3c7f2e5fc859659c2ceb3406bd4919f059c3b84d
kellel14 <kellel14@gmail.com>
2019-07-10 18:46:41 -0400

    Exercise Lesson 101

---
+++ src/main/java/com/example/demo/SpringBoot101eApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot101eApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot101eApplication.class, args);
+	}
+
+}
